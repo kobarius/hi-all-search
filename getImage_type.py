c2c3a6e3d871e702fe901dee2823baf15e5dfe97
dashimaki360 <17.punch@gmail.com>
2017-05-31 11:14:25 +0900

    hagawa commit getImage type version

--- getImage_type.py
+++ getImage_type.py
@@ -8,12 +8,12 @@ def showGoogleImgSearch(word):
 
 if __name__ == "__main__":
     
-    print "\ntype 'thank you has' if you want to stop\n"
+    print ("\ntype 'thank you has' if you want to stop\n")
     while(1):
-        print "type a word please: "
+        print ("type a word please: ")
         input_word = sys.stdin.readline()
         if input_word == "thank you has\n":
-            print "\nYou're welcome, see you again!\n"
+            print ("\nYou're welcome, see you again!\n")
             break
         showGoogleImgSearch(input_word)
         time.sleep(2)
