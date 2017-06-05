d9349a0119c1b068cb97546e4b9704829d915b3f
kobarius <covache1980@gmail.com>
2017-06-05 15:00:49 +0900

    for python 3.x

--- getAPIkey.py
+++ getAPIkey.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
 
-import ConfigParser
+import configparser
 import os
 
 def getAPIkey(API_KEY,DefStr,EnvName) :
-	if API_KEY == DefStr :
-		API_KEY = os.getenv(EnvName, '')
+    if API_KEY == DefStr :
+        API_KEY = os.getenv(EnvName, '')
 
-	if API_KEY == "":
-		API_KEY = 'API_KEY_ERROR'
+    if API_KEY == "":
+        API_KEY = 'API_KEY_ERROR'
 
-	return API_KEY
+    return API_KEY
 
 
 if __name__ == '__main__':
-	inifile = ConfigParser.SafeConfigParser()
-	inifile.read('./config.ini')
+    inifile = configparser.ConfigParser()
+    inifile.read('./config.ini')
 
-	API_KEY = inifile.get('settings', 'GOOGLE_API_KEY')
-	DefStr = 'Set_Your_Google_API_KEY'
-	EnvName = 'GOOGLE_API_KEY'
+    API_KEY = inifile['settings']['GOOGLE_API_KEY']
+    DefStr = 'Set_Your_Google_API_KEY'
+    EnvName = 'GOOGLE_API_KEY'
 
-	API_KEY = getAPIkey(API_KEY,DefStr,EnvName)
+    API_KEY = getAPIkey(API_KEY,DefStr,EnvName)
 
-	print( API_KEY )
+    print( API_KEY )
 
