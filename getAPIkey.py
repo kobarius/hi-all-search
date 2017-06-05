356533c3e033f9cd6a35b1e60f1e23e23a362070
kobarius <covache1980@gmail.com>
2017-06-05 10:55:57 +0900

    mdoify

--- getAPIkey.py
+++ getAPIkey.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import os
 import ConfigParser
+import os
 
 def getAPIkey(API_KEY,DefStr,EnvName) :
 	if API_KEY == DefStr :
 		API_KEY = os.getenv(EnvName, '')
+
+	if API_KEY == "":
+		API_KEY = 'API_KEY_ERROR'
 
 	return API_KEY
 
@@ -20,8 +23,5 @@ if __name__ == '__main__':
 
 	API_KEY = getAPIkey(API_KEY,DefStr,EnvName)
 
-	if API_KEY == '' :
-		print('API_ERROR')
-	else :
-		print( API_KEY )
+	print( API_KEY )
 
