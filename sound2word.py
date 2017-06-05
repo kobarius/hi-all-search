201cfb5a43141d4c51c6b427c1ee38db2509838e
kobarius <covache1980@gmail.com>
2017-06-05 14:49:02 +0900

    mod

--- sound2word.py
+++ sound2word.py
@@ -5,27 +5,27 @@ import ConfigParser
 import requests
 import json
 import os
-import commands
+#import commands
 #import time
 
 #LISTEN_SECONDS = 5 
-VOICE_IN_PATH = './tmp.flac'
+#VOICE_IN_PATH = './tmp.flac'
 
 # 録音
-def listen(seconds):
+def listen(seconds,VOICE_IN_PATH):
     cmdline = 'AUDIODEV=hw:1 rec -c 1 -r 11025 ' + VOICE_IN_PATH + \
         ' trim 0 ' + str(seconds)
     os.system(cmdline)
     return os.path.getsize(VOICE_IN_PATH)
 
 # 音声認識
-def recognize():
+def recognize(API_KEY,VOICE_IN_PATH):
     f = open(VOICE_IN_PATH, 'rb')
     voice = f.read()
     f.close()
 
     url = 'https://www.google.com/speech-api/v2/recognize?xjerr=1&client=chromium&'\
-        'lang=ja-JP&maxresults=10&pfilter=0&xjerr=1&key=' + GOOGLE_APIKEY
+        'lang=ja-JP&maxresults=10&pfilter=0&xjerr=1&key=' + API_KEY
     hds = {'Content-type': 'audio/x-flac; rate=11025'}
 
     try:
@@ -48,9 +48,13 @@ def recognize():
 
 
 if __name__ == '__main__':
-    #first time record will be failed. 
-    listen(5)
-    message = recognize().encode('utf-8')
+
+    inifile = ConfigParser.SafeConfigParser()
+    inifile.read('./config.ini')
+
+    API_KEY = inifile.get('settings', 'GOOGLE_API_KEY')
+    VOICE_IN_PATH = inifile.get('settings', 'VOICE_IN_PATH')
+    listen(5,VOICE_IN_PATH)
+    message = recognize(API_KEY,VOICE_IN_PATH).encode('utf-8')
     print(message)
 
-    
